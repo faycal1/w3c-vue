a9494e783e7e39ce562adb860b128c6b56b10fcb
Fayçal Hasbellaoui <hasbellaoui.faycl@gmail.com>
2021-01-29 11:22:47 +0100

    ...

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,2 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 export { default as W3cVueSample } from './w3c-vue-sample.vue';
+import W3cInput from './w3c-input/index';
+import W3cButton from './button/index';
+export {  W3cInput, W3cButton  } ;
